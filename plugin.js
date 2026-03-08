217e1302d30ab2a73db36a0b88ad611c620a6715
dragonx86dev <49553100+dragonx86dev@users.noreply.github.com>
2026-03-08 16:54:34 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -2,11 +2,21 @@
     'use strict';
 
     function start() {
-        if (window.anti_dmca_plugin) {
-            return;
-        }
+        var proxy = {
+            path_image: 'nl.imagetmdb.com/',
+            path_api: 'tmdb-api.rootu.top/3/'
+        };
+    
+        Lampa.TMDB.image = function (url) {
+            var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+            return 'http://' + (Lampa.Storage.field('proxy_tmdb') ? proxy.path_image + url : base).replace(/\/\//g, '/');
+        };
 
-        window.anti_dmca_plugin = true;
+        Lampa.TMDB.api = function (url) {
+            var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+            return 'http://' + (Lampa.Storage.field('proxy_tmdb') ? proxy.path_api + url : base).replace(/\/\//g, '/');
+        };
+
         window.lampa_settings.dcma = [];
 
         Lampa.Utils.dcma = function () { return undefined };
