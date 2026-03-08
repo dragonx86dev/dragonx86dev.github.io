84ef3304d63c5579dc2dcb49fa65d09862e29b85
dragonx86dev <49553100+dragonx86dev@users.noreply.github.com>
2026-03-08 16:34:34 +0300

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    function start() {
+        if (window.anti_dmca_plugin) {
+            return;
+        }
+
+        window.anti_dmca_plugin = true;
+        window.lampa_settings.dcma = [];
+
+        Lampa.Utils.dcma = function () { return undefined };
+        var defaultSource = Lampa.Storage.get('source', 'cub');
+
+        Lampa.Listener.follow('request_secuses', function (event) {
+            if (event.data.blocked) {
+                window.lampa_settings.dcma = [];
+
+                var active = Lampa.Activity.active();
+                active.source = 'tmdb';
+                Lampa.Storage.set('source', 'tmdb', true);
+
+                setTimeout(function () {
+                    setTimeout(function () {
+                        Lampa.Activity.replace(active);
+                        Lampa.Storage.set('source', defaultSource, true);
+                    }, 300);
+                    Lampa.Controller.toggle('content');
+                }, 250);
+            }
+        });
+    }
+
+    function startPlugin() {
+      window.plugin_antidcma_ready = true;
+      if (window.appready) {
+        start();
+      }
+      else {
+        Lampa.Listener.follow('app', function (e) {
+          if (e.type === 'ready') start();
+        });
+      }
+    }
+    if (!window.plugin_antidcma_ready) startPlugin();
+})();
