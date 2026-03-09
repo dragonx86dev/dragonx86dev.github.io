2c3f56d53291686209388a6ac56b6f5b5ffb7b9d
dragonx86dev <49553100+dragonx86dev@users.noreply.github.com>
2026-03-09 13:55:31 +0300

    Update antidcma.js

--- plugins/antidcma.js
+++ plugins/antidcma.js
@@ -1,26 +1,20 @@
 (function () {
     'use strict';
 
-    function start() {
-        var proxy = {
-            path_image: 'nl.imagetmdb.com/',
-            path_api: 'tmdb-api.rootu.top/3/'
-        };
+    var proxy = {
+        path_image: 'nl.imagetmdb.com/',
+        path_api: 'tmdb-api.rootu.top/3/'
+    };
     
-        Lampa.TMDB.image = function (url) {
-            var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
-            return 'http://' + (Lampa.Storage.field('proxy_tmdb') ? proxy.path_image + url : base).replace(/\/\//g, '/');
-        };
+    Lampa.TMDB.image = function (url) {
+        var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+        return 'http://' + (Lampa.Storage.field('proxy_tmdb') ? proxy.path_image + url : base).replace(/\/\//g, '/');
+    };
 
-        Lampa.TMDB.api = function (url) {
-            var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
-            return 'http://' + (Lampa.Storage.field('proxy_tmdb') ? proxy.path_api + url : base).replace(/\/\//g, '/');
-        };
+    Lampa.TMDB.api = function (url) {
+        var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+        return 'http://' + (Lampa.Storage.field('proxy_tmdb') ? proxy.path_api + url : base).replace(/\/\//g, '/');
+    };
 
-        window.lampa_settings.disable_features.dmca = true;
-        window.lampa_settings.dcma = [];
-        Lampa.Utils.dcma = function () { return undefined };
-    }
-
-    start();
+    window.lampa_settings.disable_features.dmca = true;
 })();
